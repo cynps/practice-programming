bc54898e79196388b4cccb8e8ab39cc0c9ec9c2c
cynps <cnospy@gmail.com>
2023-02-15 23:35:06 +0900

    init: AtCoder Beginners Selection(ABS)

---
+++ atcoder/_atcoder-beginners-selection/0-practice-a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+// using
+using namespace std;
+
+// input 
+int a,b,c;
+string s;
+
+int main() {
+  cin >> a >> b >> c >> s;
+  cout << (a+b+c) << " " << s << endl;
+  return 0;
+}
