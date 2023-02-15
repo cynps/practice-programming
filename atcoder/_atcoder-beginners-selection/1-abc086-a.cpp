369eec7b24b9b1dd158b25c8e2308a493f99eb45
cynps <cnospy@gmail.com>
2023-02-15 23:35:22 +0900

    add: abs: 1

---
+++ atcoder/_atcoder-beginners-selection/1-abc086-a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+// using
+using namespace std;
+
+// input 
+int a,b;
+
+int main() {
+  cin >> a >> b;
+  if ((a % 2) == 0 || (b % 2) == 0) cout << "Even" << endl;
+  else cout << "Odd" << endl;
+  return 0;
+}
