7b70ef22f0ffe2b398d5d59c45e6b823af947649
cynps <cnospy@gmail.com>
2023-02-24 16:39:31 +0900

    upd: template

--- tool/template.cpp
+++ tool/template.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 #define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; b<i; --i)
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 const ll mod97 = 1000000007; // 10^9+7
 
 // input 
