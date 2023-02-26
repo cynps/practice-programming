f510718dbab431d402d2ebe37712530f121b436d
cynps <cnospy@gmail.com>
2023-02-26 17:39:24 +0900

    add: atcoder abc template creator

---
+++ tool/cr_atcoder_abc.bat
@@ -0,0 +1,8 @@
+@echo off
+mkdir ..\atcoder\AtCoderBeginnerContest\%1
+copy template.cpp ..\atcoder\AtCoderBeginnerContest\%1\a.cpp
+copy template.cpp ..\atcoder\AtCoderBeginnerContest\%1\b.cpp
+copy template.cpp ..\atcoder\AtCoderBeginnerContest\%1\c.cpp
+copy template.cpp ..\atcoder\AtCoderBeginnerContest\%1\d.cpp
+copy template.cpp ..\atcoder\AtCoderBeginnerContest\%1\e.cpp
+copy g.bat ..\atcoder\AtCoderBeginnerContest\%1\g.bat
