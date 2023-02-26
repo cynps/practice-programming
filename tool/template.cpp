d63b77a9d70be5bc96e62bef424b9887d03849bb
cynps <cnospy@gmail.com>
2023-02-26 22:47:45 +0900

    upd: template

--- tool/template.cpp
+++ tool/template.cpp
@@ -4,9 +4,20 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-const ll mod = 1e9+7;
+// const ll mod = 1e9+7;
+const int mod = 998244353;
 
 int N;
+
+ll modpow(ll a, ll n, ll mod) {
+  ll res = 1;
+  while (n>0) {
+    if (n & 1) res = res * a % mod;
+    a = a * a % mod;
+    n >>= 1;
+  }
+  return res;
+}
 
 int solve() {
   int ans = 0;
