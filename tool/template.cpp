7dee944a42ac909e919fdbdddbc8c4804e84eb66
cynps <cnospy@gmail.com>
2023-03-06 23:27:00 +0900

    upd: template: union find tree

--- tool/template.cpp
+++ tool/template.cpp
@@ -9,6 +9,34 @@ const int mod = 998244353;
 
 int N;
 
+class UnionFind {
+public:
+	UnionFind() = default;
+	explicit UnionFind(size_t n)
+		: m_parents(n)
+		, m_sizes(n, 1) {
+		std::iota(m_parents.begin(), m_parents.end(), 0);
+	}
+	int find(int i) {
+		if (m_parents[i] == i) return i;
+		return (m_parents[i] = find(m_parents[i]));
+	}
+	void merge(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if (a != b) {
+			m_sizes[a] += m_sizes[b];
+			m_parents[b] = a;
+		}
+	}
+	bool connected(int a, int b) { return (find(a) == find(b)); }
+	int size(int i) { return m_sizes[find(i)]; }
+
+private:
+	std::vector<int> m_parents;
+	std::vector<int> m_sizes;
+};
+
 ll modpow(ll a, ll n, int mod) {
   ll res = 1;
   while (n>0) {
