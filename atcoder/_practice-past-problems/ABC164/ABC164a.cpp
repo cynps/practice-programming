6309078a979534ac23248f9d5b8a9fa1b520e005
cynps <cnospy@gmail.com>
2023-03-10 18:15:32 +0900

    add: abc: 164a

---
+++ atcoder/_practice-past-problems/ABC164/ABC164a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; b<i; --i)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+// const int mod = 1e9+7;
+const int mod = 998244353;
+const ld PI = 3.141592653589793;
+
+int S,W;
+
+int solve() {
+  int ans = 0;
+  return ans;
+}
+
+int main() {
+  cin >> S >> W;
+	if (S > W) cout << "safe" << endl;
+	else cout << "unsafe" << endl;
+
+  // int ans = solve();
+
+	// // cout << fixed << setprecision(20);
+  // cout << ans << endl;
+  return 0;
+}
