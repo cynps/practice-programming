b1c06c65f46fcdb20465ca7a20de92c456178137
cynps <cnospy@gmail.com>
2023-03-11 22:34:40 +0900

    add: abc: 166a

---
+++ atcoder/_practice-past-problems/ABC166/ABC166a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; b<i; --i)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+// const int mod = 1e9+7;
+const int mod = 998244353;
+const ld PI = 3.141592653589793;
+
+int N;
+string S;
+
+int solve() {
+  int ans = 0;
+  return ans;
+}
+
+int main() {
+  cin >> S;
+	string ans = "ABC";
+	if (S == ans) ans = "ARC";
+	cout << ans << endl;
+
+  return 0;
+}
