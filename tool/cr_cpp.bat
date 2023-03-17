c3b71a6c484f97cf07fb5008100b8aeaf57f01bf
cynps <cnospy@gmail.com>
2023-03-18 00:13:05 +0900

    upd: template
    * rename basic template
    * update bat file

--- tool/cr_cpp.bat
+++ tool/cr_cpp.bat
@@ -1,8 +1,8 @@
 @echo off
 mkdir .\%1
-copy ..\tool\template-abc.cpp .\%1\%1a.cpp
-copy ..\tool\template-abc.cpp .\%1\%1b.cpp
-copy ..\tool\template-abc.cpp .\%1\%1c.cpp
+copy ..\tool\template-basic.cpp .\%1\%1a.cpp
+copy ..\tool\template-basic.cpp .\%1\%1b.cpp
+copy ..\tool\template.cpp .\%1\%1c.cpp
 copy ..\tool\template.cpp .\%1\%1d.cpp
 copy ..\tool\template.cpp .\%1\%1e.cpp
 copy ..\tool\g.bat .\%1\g.bat
