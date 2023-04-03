eecbb8a957966a6ed1ff973b3cb1534e0c023edb
cynps <cnospy@gmail.com>
2023-04-03 22:38:47 +0900

    upd: template リファクタ

--- tool/template-basic.cpp
+++ tool/template-basic.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 #define rp(i,n) for (int i=0; i<(n); ++i)
 #define rep(i,a,b) for(int i=a; i<b; ++i)
 #define rrep(i,a,b) for(int i=a; b<i; --i)
-using namespace std;
 typedef long long ll;
 typedef long double ld;
 const ld PI = 3.141592653589793;
 inline int ctoi(char c) {return c-'0';}
 inline char itoc(int i) {return i+'0';}
-inline int alpint(char c) {return (int)c-64;}
-
-int N;
+inline int alpint(char c) {return (int)c-64;} // A = 1
+inline char intalp(int i) {return (char)i+64;} // 1 = A
 
 int gcd(int p, int q) {
 	if(p % q == 0) return q;
 	return gcd(q, p % q);
 }
+
+int N;
 
 int main() {
 	cin >> N;
--- tool/template.cpp
+++ tool/template.cpp
@@ -1,7 +1,4 @@
 #include <bits/stdc++.h>
-#define rp(i,n) for (int i=0; i<(n); ++i)
-#define rep(i,a,b) for(int i=a; i<b; ++i)
-#define rrep(i,a,b) for(int i=a; b<i; --i)
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull; // ull 0 - 1 = 18446 74407 37095 51615 ≒ 1.8 * 10^19
@@ -12,13 +9,16 @@ const ld PI = 3.141592653589793;
 inline int ctoi(char c) {return c-'0';}
 inline char itoc(int i) {return i+'0';}
 inline int alpint(char c) {return (int)c-64;} // A = 1
+inline char intalp(int i) {return (char)i+64;} // 1 = A
+
+#define rp(i,n) for (int i=0; i<(n); ++i)
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rrep(i,a,b) for(int i=a; b<i; --i)
 
 #define bitrep(bit,n) for(int bit=0; bit<(1<<n); ++bit)
 #define bitfind(bit,i,n) for(int i=0; i<(n); ++i) if(bit & (1<<i))
 #define bitrange(i,n,range) for(int i=0, range=1; i<(n); ++i) range *= 2
 inline int bitcount(int n) {return bitset<32>(n).count();}
-
-int N;
 
 class UnionFind {
 public:
@@ -73,6 +73,8 @@ int solve() {
 	int ans = 0;
 	return ans;
 }
+
+int N;
 
 int main() {
 	cin >> N;
