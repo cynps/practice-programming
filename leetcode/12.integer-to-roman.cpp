f3bae29b0ef4a7e71eada49d270b10d38b4720e4
cynps <cnospy@gmail.com>
2023-05-11 06:41:30 +0900

    upd: leetcode 12

--- leetcode/12.integer-to-roman.cpp
+++ leetcode/12.integer-to-roman.cpp
@@ -12,19 +12,19 @@ public:
         num -= tmp;
         num /= 10;
       }
-      int sz = digit.size();
       
-      vector<vector<string>> r(4);
-      r[0] = {"I","V","X","IV","IX"};
-      r[1] = {"X","L","C","XL","XC"};
-      r[2] = {"C","D","M","CD","CM"};
-      r[3] = {"M","-","-","-","-"};
+      string r[4][4] = {
+        {"I","V","IV","IX"},
+        {"X","L","XL","XC"},
+        {"C","D","CD","CM"},
+        {"M","-","-","-"}
+      };
 
       // for (int i=digit.size()-1; i>=0; --i) cout << digit[i] << endl;
 
       for (int i=digit.size()-1; i>=0; --i) {
-        if (digit[i] == 9) ans += r[i][4];  
-        else if (digit[i] == 4) ans += r[i][3];
+        if (digit[i] == 9) ans += r[i][3];  
+        else if (digit[i] == 4) ans += r[i][2];
         else {
           if (digit[i] >= 5) {
             ans += r[i][1]; 
