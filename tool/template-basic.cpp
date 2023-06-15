950efd888133285361207e21d9833ed65fe220b9
cynps <cnospy@gmail.com>
2023-06-16 02:43:12 +0900

    upd template

--- tool/template-basic.cpp
+++ tool/template-basic.cpp
@@ -1,37 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-const ld PI = 3.141592653589793;
-
-#define rp(i,n) for (int i=0; i<(n); ++i)
 #define rep(i,a,b) for(int i=a; i<b; ++i)
-#define rrep(i,a,b) for(int i=a; b<i; --i)
 
-inline int ctoi(char c) {return c-'0';}
-inline char itoc(int i) {return i+'0';}
-inline int alpint(char c) {return (int)c-64;} // A = 1
-inline char intalp(int i) {return (char)i+64;} // 1 = A
-
-int gcd(int p, int q) {
-	if(p % q == 0) return q;
-	return gcd(q, p % q);
-}
-
-int N;
-
-int main() {
-	cin >> N;
-
-	int ans = N;
-
-	// cout << alpint('A') << " " << alpint('Z') << endl; // 1 26
-
-	// if (ans == 1) cout << "Yes" << endl;
-	// else cout << "No" << endl;
-	// cout << fixed << setprecision(20);
-	cout << ans << endl;
-
+int main() 
+{
+	
 	return 0;
 }
--- tool/template.cpp
+++ tool/template.cpp
@@ -49,12 +49,12 @@ private:
 	std::vector<int> m_sizes;
 };
 
-ll modpow(ll a, ll n, int mod) {
-	ll res = 1;
-	while (n>0) {
-		if (n & 1) res = res * a % mod;
-		a = a * a % mod;
-		n >>= 1;
+ll modpow(ll a,ll n) {
+	ll res=1;
+	while(n>0){
+		if(n&1)res=res*a%mod;
+		a=a*a%mod;
+		n>>=1;
 	}
 	return res;
 }
