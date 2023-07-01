217696196d38269528214de614864682287d3d20
cynps <cnospy@gmail.com>
2023-07-01 21:00:32 +0900

    abc306a

---
+++ atcoder/ABC306/ABC306a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+
+int main() 
+{
+	int N;
+	string S;
+	cin>>N>>S;
+	rep(i,0,N)cout<<S[i]<<S[i];
+	cout<<endl;  
+	return 0;
+}
